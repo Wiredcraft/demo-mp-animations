c556a269e637dfdb2efd0c00268f9afed60f1c61
Mary Ma <maxiuli95@gmail.com>
2019-02-27 17:42:50 +0800

    add checkbox animation

--- src/pages/animation/checkbox.js
+++ src/pages/animation/checkbox.js
@@ -1,8 +1,12 @@
 Page({
     data:{
-      selected: false
+      selected: {},
+      colors: ['CCCC99', 'FFCCCC', '99CCCC', 'FFCC33', '66CCCC', '339999', 'CCCCCC', 'CCCCFF']
     },
-    handleItemClick() {
-      this.setData({selected: !this.data.selected});
+    handleItemClick(e) {
+      const {color} = e.currentTarget.dataset;
+      const {selected} = this.data;
+      selected[color] = !this.data.selected[color];
+      this.setData({selected});
     },
 })
